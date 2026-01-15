ad7aa6bf1f3479ba83a979b4af49c906a089087d
Richard Cyrus <richard.cyrus@rcyrus.com>
2026-01-14 21:03:10 -0500

    fix: hover ui to match design

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -16,7 +16,7 @@ export function TextField({
 }: FieldProps) {
   return (
     <div className={`group ${fieldType === 'tel' ? 'is-tel' : ''}`}>
-      <label htmlFor={name} className="flex flex-col">
+      <label htmlFor={name} className="flex flex-col hover:cursor-pointer">
         <span className="flex justify-between">
           <span className="text-xs font-normal tracking-normal text-blue-950 md:text-sm">
             {label}
@@ -30,7 +30,7 @@ export function TextField({
           name={name}
           id={name}
           placeholder={placeholder}
-          className="mt-2 min-h-10 w-full rounded-lg bg-white px-4 py-2 text-sm font-medium outline-1 -outline-offset-1 outline-purple-200 group-[.is-tel]:text-center placeholder:text-sm placeholder:font-medium placeholder:text-grey-500 invalid:outline-red-500 focus-within:text-blue-950 focus-within:outline-purple-600 md:min-h-12 md:text-base md:group-[.is-tel]:text-left md:placeholder:text-base"
+          className="mt-2 min-h-10 w-full rounded-lg bg-white px-4 py-2 text-sm font-medium outline-1 -outline-offset-1 outline-purple-200 group-[.is-tel]:text-center placeholder:text-sm placeholder:font-medium placeholder:text-grey-500 invalid:outline-red-500 focus-within:text-blue-950 focus-within:outline-purple-600 hover:cursor-pointer hover:outline-purple-600 md:min-h-12 md:text-base md:group-[.is-tel]:text-left md:placeholder:text-base"
           {...props}
         />
       </label>
