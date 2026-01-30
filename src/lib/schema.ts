a3a203081253883824621303f1e8c3d7c072580f
Richard Cyrus <richard.cyrus@rcyrus.com>
2026-01-30 17:19:50 -0500

    fix: use zod's standard email validation

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -3,10 +3,7 @@ import validator from 'validator'
 
 export const gamingPlanSchema = z.object({
   full_name: z.string().min(1, 'This field is required'),
-  email_address: z.email({
-    pattern: z.regexes.html5Email,
-    error: 'Enter a valid email address',
-  }),
+  email_address: z.email('Enter a valid email address'),
   phone_number: z.string().refine((v) => validator.isMobilePhone(v, 'any'), {
     message: 'Enter a valid phone number',
   }),
